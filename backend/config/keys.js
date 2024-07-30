b11552b62d03b869daf215ca0887ae275a99a05d
iamapoorvverma <90092702+iamapoorvverma@users.noreply.github.com>
2024-07-30 14:43:16 +0530

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://thespambot:hfdT2mpy1GNBZMwq@cluster0.secy1.mongodb.net/?retryWrites=true&w=majority",
-  secretOrKey: "secret",
+  mongoURI: 'mongodb://127.0.0.1:27017/jobforher',
+  secretOrKey: 'secret'
 };
